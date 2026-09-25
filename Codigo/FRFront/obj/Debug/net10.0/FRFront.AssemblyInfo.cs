63af7c7877d0c143fe3d7adb233bbe2a8318d112
roci0milanese <rociomilanese28@gmail.com>
2026-09-25 17:13:40 -0300

    cambios merge

--- Codigo/FRFront/obj/Debug/net10.0/FRFront.AssemblyInfo.cs
+++ Codigo/FRFront/obj/Debug/net10.0/FRFront.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FRFront")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aa146503d171a4356b2f7cd6c4243dae0a3d055")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac484faa07f7e647fd3a0c59a3ec0cce872f1fcf")]
 [assembly: System.Reflection.AssemblyProductAttribute("FRFront")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FRFront")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Codigo/FYR-API/obj/Debug/net10.0/FYR-API.AssemblyInfo.cs
+++ Codigo/FYR-API/obj/Debug/net10.0/FYR-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FYR-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aa146503d171a4356b2f7cd6c4243dae0a3d055")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac484faa07f7e647fd3a0c59a3ec0cce872f1fcf")]
 [assembly: System.Reflection.AssemblyProductAttribute("FYR-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FYR-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
